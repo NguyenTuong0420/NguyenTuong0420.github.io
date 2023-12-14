6f7a5db632683adc588f63df700758bf07fd8c35
Tuong Nguyen <manhtuonga2bd@Gmail.com>
2023-12-14 10:18:51 +0200

    Change Dec_14

--- My_first_website.js
+++ My_first_website.js
@@ -1,12 +1,12 @@
-// Function to show the popup
+
 function showPopup() {
     document.getElementById('videoPopup').style.display = 'flex';
   }
 
-  // Function to close the popup
+
   function closePopup() {
     document.getElementById('videoPopup').style.display = 'none';
   }
 
-  // Attach event listener to the button
+
   document.getElementById('video_button').addEventListener('click', showPopup)
